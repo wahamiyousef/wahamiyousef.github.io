e84aa1b06e50af0e475ad0e221e61ab71cdd509b
Yousef Al-Wahami <alwahami.yousef@gmail.com>
2024-03-31 02:07:36 -0400

    Added github link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,7 @@ function Contact() {
     <div id="contact">
         <h1>Contact Me!</h1>
         <p>Connect with me on <a href="https://www.linkedin.com/in/yousef-al-wahami">Linkedin!</a></p>
+        <p>Check out my <a href="https://github.com/wahamiyousef">Github</a></p>
     </div>
   )
 }
