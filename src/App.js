32783450180db43b347e6ee908bc36cb7d09a4cb
Yousef Al-Wahami <alwahami.yousef@gmail.com>
2024-10-01 23:30:13 -0400

    quick info

--- src/App.js
+++ src/App.js
@@ -3,21 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='background'>
+        <img src="" style={{height: '800px'}}/>
+      </div>
+      <div>
+        <div className='home'>
+          <h1>Yousef Al-Wahami</h1>
+        </div>
+
+        <div className='about'>
+          <h1>About Me</h1>
+          <p>I am currently a student at the University of Windsor studying Computer Science.</p><br />
+          <p>Here are some technologies I work with:</p>
+          <ul className='techStack'>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>Node.js</li>
+            <li>Python</li>
+            <li>Java</li>
+            <li>C#</li>
+          </ul>
+        </div>
+        
+      </div>
     </div>
   );
 }
