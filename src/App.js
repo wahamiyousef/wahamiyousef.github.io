a158560a7d36841ba652b54092b939119472b41c
Yousef Al-Wahami <alwahami.yousef@gmail.com>
2025-02-11 13:08:08 -0500

    fix color

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import './styles/meteor.css'
 import './styles/location.css'
-import Github from './assets/github.svg'
-import Linkedin from './assets/linkedin.svg'
-import Email from './assets/email.svg'
 import Blob from './assets/realblob.png'
-import nodejs from './assets/nodejs.svg'
-import ts from './assets/typescript.svg'
-import py from './assets/python.svg'
-import java from './assets/java.svg'
-import cpp from './assets/cpp.svg'
-import js from'./assets/javascript.svg'
-import mysql from './assets/mysql.png'
-import blaise from'./assets/blaise.png'
-import Card from './components/TechCard';
-import Exp from './components/Exp';
-import Button from './components/Button';
 import Meteor from './components/Meteor';
 import Home from './components/Home';
 import Experience from './components/Experience';
